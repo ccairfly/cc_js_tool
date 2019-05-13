11b3ff8cfdc50831518433e04f49175355074eb1
cc_airfly <921537102@qq.com>
2019-05-13 13:07:10 +0800

    start:demo home page

---
+++ demo_page/src/main.js
@@ -0,0 +1,15 @@
+//这是入口js 一般命名为main.js
+
+import $ from "jquery"
+
+//引入css文件直接import
+import "./css/index.css"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+$(function(){
+    
+})
+
+console.log("ok cc let go");
