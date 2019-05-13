6f09a632e405e67dd0e437d274452e685aa508be
cc <chao.pan@atxchina.cn>
2019-05-13 14:42:22 +0800

    complete checksum function

--- checksum/checksum.js
+++ checksum/checksum.js
@@ -16,7 +16,16 @@ var checksum = 0
 dataArr2.forEach(item=>{
     checksum += item
 })
-// console.log(dataArr);
-// console.log(dataArr2);
-console.log("res:"+checksum.toString(16));
-
+checksum = checksum.toString(16).padStart(4,"0")
+console.log("bigdata: " + checksum)
+var small = checksum.split("")
+console.log(small);
+//大小端转换
+var tem1 = small[0]
+var tem2 = small[1]
+small[0] = small[2]
+small[1] = small[3]
+small[2] = tem1
+small[3] = tem2
+small = small.join('')
+console.log('smalldata: ' + small)
