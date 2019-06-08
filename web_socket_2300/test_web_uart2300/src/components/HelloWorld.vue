8feda72163daa6744d66d72963215d3f507b9144
cc <chao.pan@atxchina.cn>
2019-06-08 10:20:02 +0800

    test:index page

--- web_socket_2300/test_web_uart2300/src/components/HelloWorld.vue
+++ web_socket_2300/test_web_uart2300/src/components/HelloWorld.vue
@@ -1,58 +1,87 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+     <el-button type="primary" plain class="btn" @click="selectSerial">选择串口</el-button>
+     <!-- <button class="btn">选择串口</button>
+     <button class="openBtn">打开串口</button> -->
+     <el-select v-model="value" placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.label"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <div class="openBtn">
+        <el-button type="primary" plain class="btn" @click="opentSerial">打开串口</el-button>
+        <el-checkbox v-model="checked">开启test-mode</el-checkbox>
+      </div>
+      <div class="textArea">
+        <el-input
+          type="textarea"
+          :rows="3"
+          v-model="textarea"
+          style="width:500px;">
+        </el-input>
+      </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      options: [],
+      value: '',
+      checked:true,//复选框选中状态
+      textarea:'',//文本域中显示的内容
+    }
+  },
+  methods: {
+    selectSerial(){
+      //选择串口
+      //192.168.1.102:3000/users/ports
+      // https://www.easy-mock.com/mock/5c6ad911d8bc8b31033c36cc/example/get-data-1
+      this.$http.get('http://192.168.1.102:3000/users/ports')
+      .then((res) => {
+        // console.log(res);
+        (res.data).map((item) =>{
+          // console.log(item)
+          var aobj = {
+            value : '',
+            label : ''
+          }
+          aobj.label = item
+          aobj.value = item
+          this.options.push(aobj)
+        })
+        // console.log(this.options)
+      })
+    },
+    opentSerial(){
+      // let params = {}
+      //打开串口
+      this.$http.post('http://192.168.1.102:3000/users/openSerialport',{
+        'com' : 'COM29'
+      }).then((res) => {
+        console.log(res)
+      })
+    }
+  },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.btn{
+  position: absolute;
+  left: 60px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.openBtn{
+  margin-top: 60px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.textArea{
+  margin-top: 60px;
 }
-a {
-  color: #42b983;
+.el-textarea__inner{
+  width:500px;
 }
 </style>
+
+
--- web_socket_2300/test_web_uart2300/src/main.js
+++ web_socket_2300/test_web_uart2300/src/main.js
@@ -1,6 +1,18 @@
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueResource from 'vue-resource'
+// import axios from 'axios';
+
 import Vue from 'vue'
 import App from './App.vue'
 
+// import { Button, Select } from 'element-ui';
+// Vue.use(Button)
+// Vue.use(Select)
+
+Vue.use(ElementUI);
+Vue.use(VueResource)
+// Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
